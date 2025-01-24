28cccfb12b1c28e8d9c7d0741333f6b0a94cc814
Mandraszewsky <mandrah@o2.pl>
2025-01-24 14:01:40 +0100

    triggering change detection manually via RxJS

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -9,10 +9,29 @@ import { MessagesService } from '../messages.service';
   //onPush strategy (if event or change input value occured inside this (or childs) component, run change detection mechanism):
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent {
+export class MessagesListComponent implements OnInit {
   private messagesService = inject(MessagesService);
 
   messages = this.messagesService.allMessages;
+
+  /////////////// triggering change detection mechanism manually via RxJS:
+  private changeDetectionRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    const subscription = this.messagesService.messages$.subscribe((messages) => {
+      // just for example:
+      let tempMessages = signal(messages);
+      this.messages = tempMessages;
+
+      this.changeDetectionRef.markForCheck();
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+  ///////////////
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,13 +1,18 @@
 import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
+  //RxJS variables:
+  messages$ = new BehaviorSubject<string[]>([]);
+
   private messages = signal<string[]>([]);
   allMessages = this.messages.asReadonly();
 
   addMessage(message: string) {
     this.messages.update((prevMessages) => [...prevMessages, message]);
+    this.messages$.next([...this.messages()]);
   }
 }
